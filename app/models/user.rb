b1f439c628e934a807ab61aa04b4a6b896c57643
Adam D. White <adamwhite82@gmail.com>
2017-12-31 16:56:44 -0600

    established sessions for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
 
-  attr_accessor :email, :password, :password_confirmation
+  # attr_accessor :email, :password, :password_confirmation
 
-  # has_secure_password
+  has_secure_password
   
+  # presence_in :session
+
   before_save { self.email = email.downcase }
   
   # def initialize(attributes = {})
   #   @email  = :email
-  #   @password = :password
+  #   @password = :password  
   #   @password_confirmation = :password_confirmation
   # end
 
@@ -17,9 +19,17 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
+  
+  validates :password, presence: true, length: { minimum: 6 }
 
   def formatted_email
       "#{@name} <#{@email}>"
   end
   
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+
 end
