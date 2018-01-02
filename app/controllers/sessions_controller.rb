2cccac12a1d23a96f77a53f13d5f6b0bd5867535
Adam D. White <adamwhite82@gmail.com>
2018-01-02 16:46:17 -0600

    developed profile update. began ACtion Cable chat functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,24 @@ class SessionsController < ApplicationController
   def new
   end
     
-  def create 
-    
-    # if user exists and matches in database:
-    user = User.find_by_email(params[:email])
-    
-    
-    if user && user.authenticate(params[:password])
-      # Log the user in and redirect to the user's show page.
-      session[:user_id] = user.id
-      log_in user
-      redirect_to user, :notice => 'Welcome back.'
+  def create
 
+    # if user exists and matches in database:
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       # Present error message.
-      flash.now[:danger] = 'Invalid email/password combination. Please try again.' 
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
+  end  
+
+
     
   def destroy
     log_out
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
   
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def formatted_email
       "#{@name} <#{@email}>"
@@ -31,5 +31,5 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-
+  end
 end
